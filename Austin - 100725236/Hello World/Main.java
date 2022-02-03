312f6611129ffb79265698e8cb85ae9b2911ee34
Austin Page <63081915+a-c-page@users.noreply.github.com>
2022-02-03 10:09:18 -0500

    Adding Code Files

---
+++ Austin - 100725236/Hello World/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int count = 0;
+        try {
+            while (true) {
+                Thread.sleep(2 * 1000);
+                System.out.println("I am still here!: " + count++);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
